4384c5cae9453804e59b99dc8df1e475997129f5
game110pm <game110pm@605-110-STU20>
2017-05-02 13:11:48 -0600

    code

---
+++ food!! 5-2/food!! 5-2/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+string arr[7] = { "chicken" " pizza" " hotdogs" " sandwitch" " chillie" " soup" " fish." };
+int main() {
+
+	for (int i = 0; i < 7; i++) {
+		cout << arr[i];
+
+
+
+
+	}
+}
+
+
+
+
